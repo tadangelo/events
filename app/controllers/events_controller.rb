8af7258540587137da47d3507ccf7f6d94ac6352
kadenjenson <sobeit99@gmail.com>
2018-01-05 13:05:51 -0700

    refactored

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,16 +1,20 @@
 class EventsController < ApplicationController
-  class EventsController < ApplicationController
-    before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  
   def index
     @events = Event.all
   end
+  
   def show
   end
+  
   def new
     @event = Event.new
   end
+  
   def edit
   end
+  
   def create
     @event = Event.new(event_params)
     respond_to do |format|
@@ -23,6 +27,7 @@ class EventsController < ApplicationController
       end
     end
   end
+  
   def update
     respond_to do |format|
       if @event.update(event_params)
@@ -32,18 +37,22 @@ class EventsController < ApplicationController
       end
     end
   end
+  
   def destroy
     @event.destroy
     respond_to do |format|
       format.html { redirect_to events_url, notice: 'Your event was deleted.' }
     end
   end
+  
   private
+    
     def set_event
       @event = Event.find(params[:id])
     end
+    
     def event_params
       params.require(:event).permit(:title, :body)
     end
-end
+
 end
