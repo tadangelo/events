e52c0dc7fe9765305749df7489132e961b8014d7
Jason Dudley <jason.dudley27@gmail.com>
2018-01-05 11:22:15 -0700

    added seeds data

--- Gemfile
+++ Gemfile
@@ -49,4 +49,4 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-gem 'ruby', '>= 2.4.0'
+gem 'faker', '~> 1.6', '>= 1.6.6'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105174435) do
+ActiveRecord::Schema.define(version: 20180105180908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20180105174435) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.text "location"
+    t.date "date"
+    t.text "thumb_image"
   end
 
 end
